f5379cce662150c407925f5f76d3b3ebb8c651df
Mahmud <mardhaka67@gmail.com>
2023-11-16 09:25:39 +0000

    new way to try

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,14 @@
-import json
 import secrets
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 app = Flask(__name__)
-with open('config.json', 'r') as c:
-    data = json.load(c)
-app.config['SQLALCHEMY_DATABASE_URI'] = data['uri']  # "sqlite:////dataßase.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import forms, models, routes
+from . import routes, models, forms
--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,9 @@ from wtforms import StringField, SubmitField, TextAreaField, PasswordField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.validators import DataRequired, Length, ValidationError
 
-
 class BookForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    book_name = StringField('name', validators=[DataRequired()])
+    author = StringField('author', validators=[DataRequired()])
     password = PasswordField(
         'password',
         validators=[
@@ -19,18 +19,4 @@ class BookForm(FlaskForm):
             FileRequired(), FileAllowed(
                 {'pdf'}, 'PDFs only!')])
     description = TextAreaField('description', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-
-class AssetForm(FlaskForm):
-    password = PasswordField(
-        'password',
-        validators=[
-            DataRequired(),
-            Length(
-                min=8,
-                max=16,
-                message='Password must be between 8 and 16 characters long!')])
-    img = FileField('img', validators=[FileRequired(), FileAllowed(
-        {'png', 'jpg', 'jpeg'}, 'Images only!')])
     submit = SubmitField('Submit')
