ea3747885607e8bb6ab51a7d88b22b281feea773
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-11-19 08:38:17 +0000

    Update database URI in app config

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,15 @@
 import secrets
-import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-with open('config.json', 'r', encoding='utf-8') as c:
-    data = json.load(c)
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # 200MB example limit
-app.config['SQLALCHEMY_DATABASE_URI'] = data['uri2']#'sqlite:///books.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres.civqwbtmpkofhrolcfar:M7jpHdxtA)zgS:@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres" # 'sqlite:///books.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secrets.token_hex(16)
+#app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'client_encoding': 'utf8'}
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
