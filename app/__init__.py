1e4673b7d003a909665c934db4c2891b4836aff0
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-12-13 23:05:17 +0000

    Waiting for a database

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # 200MB example limit
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://MAHMUD1223:qZ5gMcIRDJl3@ep-plain-sky-804803-pooler.us-east-2.aws.neon.tech/book?sslmode=require"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 db = SQLAlchemy(app)
