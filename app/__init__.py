fb5b753eb7cfde9427aae3c4f3ac0e95c784f6db
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-12-14 10:13:07 +0000

    making good change for freking damm

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 import secrets
+import json
 
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+with open('sj', 'r') as c:
+    data = json.load(c)
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # 200MB example limit
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = data['in'][::-1]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 db = SQLAlchemy(app)
---
+++ sj
@@ -0,0 +1,4 @@
+{
+    "in": "eriuqer=edomlss?sergtsop/2345:moc.eruza.esabatad.sergtsop.koob@:Sgz)AtxdHpj7M:dumhaM//:lqsergtsop",
+    "pswd": "nopassword"
+}
