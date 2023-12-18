e25cd57abc1a15c19a662c98d05eeb985c5b89a7
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2023-12-18 02:11:26 +0000

    Add Flask minify and implement book editing functionality

--- app/forms.py
+++ app/forms.py
@@ -21,3 +21,19 @@ class BookForm(FlaskForm):
                 {'pdf'}, 'PDFs only!')])
     description = TextAreaField('description', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class BookEdit(FlaskForm):
+    id = StringField('id')
+    book_name = StringField('name', validators=[DataRequired()])
+    author = StringField('author', validators=[DataRequired()])
+    password = PasswordField(
+        'password',
+        validators=[
+            DataRequired(),
+            Length(
+                min=8,
+                max=16,
+                message='Password must be between 8 and 16 characters long!')])
+    description = StringField('description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
